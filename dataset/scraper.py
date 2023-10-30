4b9c441854a3ca454fd8d8bd2715d7c86368c89c
Samuel Elmer <sam.elmer@icloud.com>
2023-10-30 18:37:19 -0500

    Remove unknown dates + sanitize data

--- dataset/scraper.py
+++ dataset/scraper.py
@@ -1,6 +1,7 @@
 # This script finds liquipedia links in players.txt, traverses them to find team history, and stores it in a CSV file.
 import requests
 from bs4 import BeautifulSoup
+from datetime import datetime
 
 URL = "https://liquipedia.net/counterstrike/MalbsMd"
 r = requests.get(URL)
@@ -9,7 +10,7 @@ soup = BeautifulSoup(r.content, "html.parser")
 
 teams = soup.find_all("div", attrs={"style": "font-size:100%; line-height:-10px;"})
 
-ALLOWEDCHARS = [" ", "-", "?"]
+ALLOWEDCHARS = ["?"]
 
 for team in teams:
     date = team.findChildren("i")
@@ -18,7 +19,16 @@ for team in teams:
         len(date) > 1
     ):  # This indicates a player was inactive/benched on that team at the time
         continue
-    date = "".join([c for c in date[0].text if c.isalnum() or c in ALLOWEDCHARS])
-    name = "".join([c for c in name[0].text if c.isalnum() or c in ALLOWEDCHARS])
+    parsedDate = "".join([c for c in date[0].text if c.isalnum() or c in ALLOWEDCHARS])
+    parsedName = "".join(
+        [c for c in name[0].text if c.isalnum() or c in ALLOWEDCHARS or c == " "]
+    )
 
-    print(name, date)
+    try:
+        joinDate = datetime.strptime(parsedDate[0:7], "%Y%m%d")
+        leaveDate = datetime.strptime(parsedDate[-8:], "%Y%m%d")
+        print(parsedName, joinDate, leaveDate)
+    except Exception as e:
+        print(e)
+
+    print()
